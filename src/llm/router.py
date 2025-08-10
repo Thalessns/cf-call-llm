70511b720f1ceb8656491ad979ee8e4655f0624c
Thales Nogueira <tthales08@gmail.com>
2025-08-10 16:33:56 -0300

    Arrumando docstring

--- src/llm/router.py
+++ src/llm/router.py
@@ -4,14 +4,10 @@ from src.llm.schemas import RequestLLM, LLMResponse
 from src.llm.prompts import Prompts
 from src.llm.client import gemini_client
 
-data = RequestLLM(
-        model="gemini-2.5-flash", contents="Qual é a maior estão de trem de são paulo?"
-    )
-
 llm_router = APIRouter(prefix="/llm")
 
 
 @llm_router.post("/completions", status_code=status.HTTP_201_CREATED)
 async def completions(data: RequestLLM) -> LLMResponse:
-    """"""
+    """This route handle llm requests"""
     return gemini_client.request_llm(data=data, prompt=Prompts.CLEAN_REQUEST)
